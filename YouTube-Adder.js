9f5fc2b2eac09084cb51c1c4bb0d7a1e17c03cca
GlavVlad <v.glavatskih@gmail.com>
2017-04-17 23:25:35 +0300

    updated for new youtube black theme

--- YouTube-Adder.js
+++ YouTube-Adder.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         YouTube Adder
 // @namespace    https://github.com/GlavVlad
-// @version      0.1
-// @description  Automatically add all videos from your "Subscriptions" page to "Watch Later" playlist (need some time)
+// @version      0.2
+// @description  Automatically add all videos from your "Subscriptions" page to "Watch Later" playlist (need some time). For black theme
 // @license      WTFPl
 // @author       GlavVlad
 // @match        https://www.youtube.com/feed/subscriptions
@@ -10,17 +10,17 @@
 // @updateURL    https://raw.githubusercontent.com/GlavVlad/YouTube-Adder/master/YouTube-Adder.js
 // ==/UserScript==
 
-var collection = document.getElementsByClassName('shelf-title-cell');
+var collection = document.getElementById('spacer');
 var btn = document.createElement('input');
 btn.setAttribute('type','button');
 btn.setAttribute('name','button');
 btn.setAttribute('value','Add All');
 btn.addEventListener('click', function add(){
-    var el = document.getElementsByClassName('addto-watch-later-button');
+    var el = document.querySelectorAll('[aria-label="Watch later"]');
     var i = el.length;
     if (i > 0) {
         el[i-1].click();
-        setTimeout(add, 100);
+        setTimeout(add, 200);
     }
 });
-collection.item(0).appendChild(btn);
+collection.appendChild(btn);
